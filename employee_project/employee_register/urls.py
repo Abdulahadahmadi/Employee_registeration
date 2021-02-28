c2a3c863e7c97f50859678f9f03b7ed100ef8cae
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-02-28 12:00:20 +0430

    some feature added

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -2,8 +2,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form, name='employee_insert'),          # for get and post request. for insert operation. 
-    path('<int:id>/', views.employee_form,name='employee_update'),  # for  get and post request. for update operatiion.
-    path('delete/<int:id>/',views.employee_delete,name='employee_delete'), # for deleting operation.
-    path('list/',views.employee_list, name='employee_list')         # for get request. to retrive nad display all records.
+   # for get request. to retrive nad display all records.
 ]
