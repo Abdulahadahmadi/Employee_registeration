299be04e0cf4be3d58aeb558918725c988864c63
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-02-28 12:00:57 +0430

    urls path addded to url sections

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   # for get request. to retrive nad display all records.
+    path('', views.employee_form, name='employee_insert'),          # for get and post request. for insert operation. 
+    path('<int:id>/', views.employee_form,name='employee_update'),  # for  get and post request. for update operatiion.
+    path('delete/<int:id>/',views.employee_delete,name='employee_delete'), # for deleting operation.
+    path('list/',views.employee_list, name='employee_list')         # for get request. to retrive nad display all records.
 ]
